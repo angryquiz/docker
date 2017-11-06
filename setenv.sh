d81ab16513803e3467fb626727b9418d849ff01f
angryquiz <33397245+angryquiz@users.noreply.github.com>
2017-11-05 23:41:49 -0600

    Create setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#https://gist.github.com/terrancesnyder/986029
+
+export JAVA_OPTS="$JAVA_OPTS -DredisServer=angryquizhost -DredisPort=6379 -DquestionRestEndpoint=http://angryquizhost:8080/question-rest -DelasticSearchUrl=http://angryquizhost:9200 "
+
+echo "Using CATALINA_OPTS:"
+for arg in $CATALINA_OPTS
+do
+    echo ">> " $arg
+done
+echo ""
+
+echo "Using JAVA_OPTS:"
+for arg in $JAVA_OPTS
+do
+    echo ">> " $arg
+done
+echo "_______________________________________________"
+echo ""
